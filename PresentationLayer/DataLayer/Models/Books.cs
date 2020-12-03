170d762c1e86f78fd5b17bfe32e6276bcab7e225
Veljko Joksimovic <joksimovicvel@gmail.com>
2020-12-03 17:59:14 +0100

    Dodata troslojna arhitektura i napravljena model klasa.

---
+++ PresentationLayer/DataLayer/Models/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Books
+    {
+        private int Id { get; set; }
+        private String Name { get; set; }
+        private String Author { get; set; }
+
+        private int Year { get; set; }
+    }
+}
