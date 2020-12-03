2902c33c0b43506f6489539e031afaedf98b19eb
Milos Manojlovic <milosh.man@gmail.com>
2020-12-03 18:26:52 +0100

    Kreiran business layer, i books forma

---
+++ WebApplication1/WebApplication1/books.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="books.aspx.cs" Inherits="WebApplication1.books" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+        <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+        <asp:Label ID="Label2" runat="server" Text="Author"></asp:Label>
+        <asp:TextBox ID="tbAuthor" runat="server"></asp:TextBox>
+        <asp:Label ID="Label3" runat="server" Text="Year"></asp:Label>
+        <asp:TextBox ID="tbYear" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSubmit" runat="server" Text="Potvrdi" OnClick="btnSubmit_Click" />
+        <asp:ListBox ID="ListBoxBooks" runat="server"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/books.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLayer;
+using DataLayer;
+
+namespace WebApplication1
+{
+    public partial class books : System.Web.UI.Page
+    {
+        private BookBusiness bookBusiness;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            refreshList();
+        }
+
+        private void refreshList()
+        {
+            List<Book> books = bookBusiness.GetAllBooks();
+            ListBoxBooks.Items.Clear();
+
+            foreach (Book book in books)
+                ListBoxBooks.Items.Add($"{book.Id}. {book.Name} {book.Author} ({book.Year})");
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        { 
+            Book book = new Book {
+                Name = tbName.Text,
+                Author = tbAuthor.Text,
+                Year = Convert.ToInt32(tbYear.Text)
+            };
+
+            if(bookBusiness.insertBook(book)) {
+                List<Book> books = bookBusiness.GetAllBooks();
+                ListBoxBooks.Items.Clear();
+
+                foreach (Book b in books)
+                    ListBoxBooks.Items.Add($"{b.Id}. {b.Name} {b.Author} ({b.Year})");
+            }
+            
+
+        }
+    }
+}
